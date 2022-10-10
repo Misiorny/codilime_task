d42968a062bb9e7cacd7754aeed7e3ad6e7b733a
Misiorny <krzystan@wp.pl>
2022-10-10 20:07:16 +0200

    feat: before add file-loader according to fix problem of loading images

---
+++ src/utilities/dataSvgIcons.ts
@@ -0,0 +1,105 @@
+const dataSvgIcons = [
+  {
+	id: 1,
+	src: '../../images/svg/android.svg',
+	nameIcon: 'androidIcon',
+	alt: 'Android Icon'
+  },
+  {
+	id: 2,
+	src: '../../images/svg/checkcircle.svg',
+	nameIcon: 'checkcircleIcon',
+	alt: 'Checked Circle Icon'
+  },
+  {
+	id: 3,
+	src: '../../images/svg/circle.svg',
+	nameIcon: 'circleIcon',
+	alt: 'Circle Icon'
+  },
+  {
+	id: 4,
+	src: '../../images/svg/codilime.svg',
+	nameIcon: 'codilimeIcon',
+	alt: 'Codilime Icon'
+  },
+  {
+	id: 5,
+	src: '../../images/svg/fb.svg',
+	nameIcon: 'fbIcon',
+	alt: 'Facebook Icon'
+  },
+  {
+	id: 6,
+	src: '../../images/svg/github.svg',
+	nameIcon: 'githubIcon',
+	alt: 'Github Icon'
+  },
+  {
+	id: 7,
+	src: '../../images/svg/instagram.svg',
+	nameIcon: 'instagramIcon',
+	alt: 'Instagram Icon'
+  },
+  {
+	id: 8,
+	src: '../../images/svg/logo.svg',
+	nameIcon: 'logoIcon',
+	alt: 'Logo Icon'
+  },
+  {
+	id: 9,
+	src: '../../images/svg/logo_invision.svg',
+	nameIcon: 'logoInvisionIcon',
+	alt: 'Invision Logo Icon'
+  },
+  {
+	id: 10,
+	src: '../../images/svg/m.svg',
+	nameIcon: 'mIcon',
+	alt: 'Letter M Icon'
+  },
+  {
+	id: 11,
+	src: '../../images/svg/minus.svg',
+	nameIcon: 'minusIcon',
+	alt: 'Minus Icon'
+  },
+  {
+	id: 12,
+	src: '../../images/svg/plus.svg',
+	nameIcon: 'plusIcon',
+	alt: 'Plus Icon'
+  },
+  {
+	id: 13,
+	src: '../../images/svg/quotedown.svg',
+	nameIcon: 'quotedownIcon',
+	alt: 'Quote down Icon'
+  },
+  {
+	id: 14,
+	src: '../../images/svg/quoteup.svg',
+	nameIcon: 'quoteupIcon',
+	alt: 'Quote up Icon'
+  },
+  {
+	id: 15,
+	src: '../../images/svg/rightarrow.svg',
+	nameIcon: 'rightarrowIcon',
+	alt: 'Right Arrow Icon'
+  },
+  {
+	id: 16,
+	src: '../../images/svg/scissors.svg',
+	nameIcon: 'scisorsIcon',
+	alt: 'Scissor Icon'
+  },
+  {
+	id: 17,
+	src: '../../images/svg/tweeter.svg',
+	nameIcon: 'tweeterIcon',
+	alt: 'Tweeter Icon'
+  }
+];
+export default dataSvgIcons;
